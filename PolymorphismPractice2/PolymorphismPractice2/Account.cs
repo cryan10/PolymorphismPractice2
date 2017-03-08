48cec638bb557afc42ee0707aa498befc18a6772
Carrie Ryan <cryan1205@gmail.com>
2017-03-07 21:56:20 -0500

    initial commit

---
+++ PolymorphismPractice2/PolymorphismPractice2/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice2
+{
+   public class Account //parent class
+    {
+        int balance;
+       
+
+        public Account()
+        {
+            //constructor
+        }
+
+        public virtual void Withdrawal()
+        {
+            //withdraw money from bank account
+            Console.WriteLine("How much money do you want to withdraw?");
+        }
+
+        public int WriteBalance()
+        {
+            Console.WriteLine("Your current balance is $", balance);
+            return balance;
+
+        }
+    }
+}
---
+++ PolymorphismPractice2/PolymorphismPractice2/Savings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice2
+{
+    public class Savings : Account
+    {
+        int minimumBalance;
+        int overdraft;
+        int interest;
+
+        public Savings()
+        {
+            //constructor
+            base.WriteBalance();
+        }
+
+        public int AddInterest()
+        {
+            return interest;
+        }
+    }
+}
